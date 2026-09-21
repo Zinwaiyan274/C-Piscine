8b06d282938f7227a0c9454b88c751dbc0df92b8
Zinwaiyan274 <zinwaiyan274@gmail.com>
2026-09-21 07:24:04 +0700

    solved ac/ag sort params

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -1,24 +1,57 @@
 #include <unistd.h>
 
-int main(int ac, char **av)
+int ft_strcmp(char *a, char *b)
 {
     int i;
+
+    i = 0;
+    while (a[i] != '\0' && b[i] != '\0')
+    {
+        if ( a[i] != b[i])
+        {
+            return (a[i] - b[i]);
+        }
+        i++;
+    }
+    return (a[i] - b[i]);
+}
+
+int main(int ac, char **av)
+{
+
+    int i;
     int j;
+    char *t;
 
     i = 1;
     j = 0;
-    ac -=1;
 
-    while ( i >= ac )
+    while ( i < ac )
     {
-        if ()
-        {
-            while (av[][j])
+        j = i + 1;
+        while (j < ac)
+        {   
+            if (ft_strcmp(av[i], av[j]) > 0)
             {
-                write(1, &av[][j], 1);
-                j++;
+                t = av[i];
+                av[i] = av[j];
+                av[j] = t; 
             }
-            write(1, "\n", 1);
-            i++;
+            j++;
         }
+        i++;
     }
+
+    i = 1;
+    while (i < ac)
+    {
+        j = 0;
+        while (av[i][j] != '\0')
+        {
+            write(1, &av[i][j], 1);
+            j++;
+        }
+        write(1, "\n", 1);
+        i++;
+    }
+}
